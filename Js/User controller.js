aa818848fc8ef21daa06eda9306d1b311d3471bc
Ray-Palmer <sudeerachandrajith1212@gmail.com>
2022-05-13 05:56:35 +0530

    Sign in half complete

--- Js/User controller.js
+++ Js/User controller.js
@@ -1,21 +1,28 @@
-const overlay = document.getElementById('btn');
+const toggleButton = document.getElementById('btn');
 const signUp = document.getElementById('signUp');
 const signIn = document.getElementById('signIn');
+const signUpForm = document.querySelector('.SignUp-Input-feild');
+const signInForm = document.querySelector('.SignIn-Input-feild');
 
 
 signUp.addEventListener('click', () => {
-    // overlay.style.background = "linear-gradient(to right,  #009ffd,#2a2a72)";
-    overlay.style.left = "0px";
+    toggleButton.style.left = "0px";
     signUp.style.color = "white";
     signIn.style.color = "black";
-    signIn.style.transition = "0.5s"
-
+    signIn.style.transition = "0.5s";
+    signUpForm.style.transition = "1s";
+    signUpForm.style.left = "12px";
+    signInForm.style.left = "-500px";
+    signInForm.style.left = "12px";
 });
 
 signIn.addEventListener('click', () => {
-    // overlay.style.background = "linear-gradient(to right, #2a2a72, #009ffd)";
-    overlay.style.left = "150px";
+    toggleButton.style.left = "150px";
     signIn.style.color = "white";
     signUp.style.color = "black";
     signUp.style.transition = "0.5s"
+    signUpForm.style.transition = "1s";
+    signUpForm.style.left = "-500px";
+
+    signInForm.style.left = "-500px";
 });
