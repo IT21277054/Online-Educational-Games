f8ec0e13500f96bea0615d944f14b3a190039edc
Ray-Palmer <sudeerachandrajith1212@gmail.com>
2022-05-15 08:49:02 +0530

    a

--- Js/index.js
+++ Js/index.js
@@ -1,19 +1,17 @@
-console.clear();
-console.log('Hello');
-
-let imageArray = ['../Images/Slideshow/img1.jpg', '../Images/Slideshow/img2.jpg', '../Images/Slideshow/img3.jpg'];
+const imageArray = ['../Images/Slideshow/img1.jpg', '../Images/Slideshow/img2.jpg', '../Images/Slideshow/img3.jpg'];
+const img = document.querySelector('#slider-image');
 let i = 0;
 
+
 function slideShow() {
-    document.getElementById('slider-image').className = 'trans';
+    img.classList.add('trans');
 
     setTimeout(function () {
-        document.getElementById('slider-image').src = imageArray[i];
-        document.getElementById('slider-image').className = "";
+        img.src = imageArray[i];
+        img.classList.remove('trans');
     }, 1000);
 
     i++
-    console.log(i);
     if (i == imageArray.length) { i = 0; }
 
     setTimeout('slideShow()', 5000);
