23d186bb47019964c7ddd8cdba38408e7f991f5d
Umayangana <89300506+Umayangana@users.noreply.github.com>
2022-05-22 23:30:47 +0530

    Nav

--- Php/MyFriends.php
+++ Php/MyFriends.php
@@ -71,13 +71,13 @@ if(isset($_POST['Remove'])){
 <header>
     <ul id="Nav-bar-id" class="Nav-bar">
         <img src="..\images\Logo\final.png" id="logo">
-        <a href="Logout.php" class="Nav-button"><li>Logout</li></a>
-        <a href="MyAccount.php" class="Nav-button"><li>My Account</li></a>
-        <a href="../Html/Contact.html" class="Nav-button"><li>Contact</li></a>
-        <a href="../Html/Friends.html" class="Nav-button"><li>Friends</li></a>
-        <a href="../Html/About.html" class="Nav-button"><li>About</li></a>
-        <a href="Games.php" class="Nav-button"><li>Games</li></a>
-        <a href="index.php" class="Nav-button"><li>Home</li></a>
+        <a href="Logout.php" class="VNav-button"><li>Logout</li></a>
+        <a href="MyAccount.php" class="VNav-button"><li>My Account</li></a>
+        <a href="../Html/Contact.html" class="VNav-button"><li>Contact</li></a>
+        <a href="../Html/Friends.html" class="VNav-button"><li>Friends</li></a>
+        <a href="../Html/About.html" class="VNav-button"><li>About</li></a>
+        <a href="Games.php" class="VNav-button"><li>Games</li></a>
+        <a href="index.php" class="VNav-button"><li>Home</li></a>
 
         </ul>
 </header>
