bf49e6851ef557c537642c26a57b7d3153a67ef5
Shmary Shiraz <shamrishiraz01@gmail.com>
2022-05-23 09:53:01 +0530

    12

---
+++ Php/SonicLSbuy.php
@@ -0,0 +1,24 @@
+<?php
+// include db connection
+include './dbConnection.php';
+
+// start session
+session_start();
+$email = $_SESSION['email'];
+
+
+
+$sql = "SELECT * FROM client WHERE Email = '$email'";
+$result = mysqli_query($conn,$sql);
+$row = mysqli_fetch_assoc($result);
+$clientID = $row['ClientID'];
+
+$_SESSION['GameID'] = 1000; // 
+
+$sql = "SELECT * FROM own WHERE ClientID = '$clientID' AND GameID = 1000";
+$result = mysqli_query($conn,$sql);
+
+echo "<script>location.replace('../Php/MyCart.php')</script>";
+
+
+?>
